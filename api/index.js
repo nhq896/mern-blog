62c57a7df1305683136d880d32d9fd9259b2fb01
Nguyen Huu Quy <nhq9689@gmail.com>
2024-03-18 22:42:38 +0700

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import exress from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -18,8 +19,11 @@ const app = exress();
 
 const port = 3000;
 
+app.use(exress.json());
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
