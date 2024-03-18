497c8a115ab1bb208af1b35fcfb591c89f01ddc8
Nguyen Huu Quy <nhq9689@gmail.com>
2024-03-18 21:00:43 +0700

    create user model

--- api/index.js
+++ api/index.js
@@ -17,10 +17,6 @@ const app = exress();
 
 const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello");
-// });
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
